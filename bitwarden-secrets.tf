12320dd37b9cb73a5cc6346e4adb998df727dccf
Kristo Aun <kristoaun@live.com>
2024-02-25 00:04:32 +0200

    run

--- bitwarden-secrets.tf
+++ bitwarden-secrets.tf
@@ -1,7 +1,7 @@
 locals {
   bw_k3s_provider_config_id = {
     snb = "1721d632-8ef6-4398-9fd5-b12000dfed36"
-    dev = "b7c5eb11-f31e-48b6-b977-b12000dfea02"
+    dev = "49f8b087-1412-4e96-9361-b1200161b670"
   }
 }
 
